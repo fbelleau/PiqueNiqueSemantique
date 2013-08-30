ebc03495dacfabf0036a95c041402635f3cf7e09
Maxime Déraspe <maximilien1er@gmail.com>
2013-08-30 01:38:01 -0400

    Merged typeAhead and describe branches
    Signed-off-by: Maxime Déraspe <maximilien1er@gmail.com>

--- LivreOuvert/js/utils.js
+++ LivreOuvert/js/utils.js
@@ -2,23 +2,26 @@
 // Fonction utilitaire qui met la premiere lettre du mot en majuscule
 function capitalize(s)
 {
-	return s[0].toUpperCase() + s.slice(1);
+  return s[0].toUpperCase() + s.slice(1);
 }
 // Fonction qui cache la mise en page qui varie et qui supprime tout ce qui est dans content
 function refreshPage() {
-	$("#content").html("");
-	// $("#searchMenu").hide();
-	$("#logo").hide(); // EVENTUELEMENT METTRE LE LOGO DU ENDPOINT
-	$("#describeHeader").hide();
-	$(".navigationButton").attr("disabled", false);
+  $("#content").html("");
+  // $("#searchMenu").hide();
+  $("#logo").hide(); // EVENTUELEMENT METTRE LE LOGO DU ENDPOINT
+  $("#describeHeader").hide();
+  $(".navigationButton").attr("disabled", false);
 }
 
 // Fait la mise en page pour le describe
 function describeContent(id,endpoint){
 
-	refreshPage();
-	$("#describeHeader").show();
-	$("#navigationButtons").show();
-	describe(id);
+  refreshPage();
+  $("#describeHeader").show();
+  $("#navigationButtons").show();
+  
+  uri = "http://192.168.130.107:9000/describe/" + id = "/?callback=?"
+
+  describe(uri);
 
 }
